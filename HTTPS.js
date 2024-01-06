50aee6ec4d528e8947e4fafbfc58951ea3d6f46f
Anuj Roy <148977861+Notanujroy@users.noreply.github.com>
2024-01-06 12:15:39 +0530

    Add files via upload

--- HTTPS.js
+++ HTTPS.js
@@ -1,5 +1,5 @@
 // Method DDoS Layer7 Leaked By  t.me/LekaingCode
-/ node HTTPS.js target time Rate threads proxyFile
+// node HTTPS.js target time Rate threads proxyFile
 
 const net = require("net");
  const http2 = require("http2");
